52f397a7325210f194df8be8471c441ca47ad04a
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-11-26 19:22:52 +0500

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap"
-import { ScrollTrigger , SplitText } from "gsap/all"
+import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar"
 
-gsap.registerPlugin(ScrollTrigger ,SplitText )
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className='flex-center h-screen'>
-      app
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
