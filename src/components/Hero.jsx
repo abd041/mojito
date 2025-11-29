3d125eb961c23943fd70d360e51edfdcb029ef8b
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-11-29 19:26:00 +0500

    react responsive installed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { SplitText , ScrollTrigger} from "gsap/all";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
@@ -35,32 +41,66 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
-  }, []);
-  return (
-    <section id="hero" className="noisy">
-      <h1 className="title">MOJITO</h1>
-      <img src="/images/hero-left-leaf.png" alt="..." className="left-leaf" />
-      <img src="/images/hero-right-leaf.png" alt="..." className="right-leaf" />
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block ">
-            <p>Cool. Crisp. Classic.</p>
-            <p className="subtitle">
-              Slip the Spirit <br /> of Summer
-            </p>
-          </div>
 
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktail on our menu is blend of premium ingredients ,
-              creative , flair , and timeless recipes -- designed to delight
-              your success
-            </p>
-            <a href="#cocktails">View Cocktails</a>
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: videoRef.current, // fixed
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+
+        <img src="/images/hero-left-leaf.png" className="left-leaf" />
+        <img src="/images/hero-right-leaf.png" className="right-leaf" />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Slip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        ></video>
       </div>
-    </section>
+    </>
   );
 };
 
