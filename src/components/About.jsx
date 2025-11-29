294e6cb55124c92867d9e92ec83ea2064a1602b4
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-11-29 20:07:46 +0500

    implement about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,70 +1,94 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const About = () => {
-    return (
-        <section id='about'>
-            <div className='mb-16 md:px-0 px-5'>
-                <div className='content'>
-                    <div className='md:col-span-8'>
-                        <p className='badge'>Best Cocktails</p>
-                        <h2>where every details matters <span className='text-white'>
-                            -</span>
-                            from muddle to granish
-                        </h2>
-                    </div>
+  useGSAP(() => {
+    const titleSpilt = SplitText.create("#about h2", {
+      type: "words",
+    });
 
-                    <div className='sub-content'>
-                        <p>
-                            Every cocktail we serve is a reflection of our obsession with detail - from the first muddle to the final granish. That care is what turns a simple drink into something truly memorable
-                        </p>
-                        <div >
-                            <p className='md:text-3xl text-xl font-bold'>
-                                <span>4.5</span>/5
-                            </p>
-                            <p className=' text-sm text-white-100'>
-                                more than +12000  customers
-                            </p>
-                        </div>
-                    </div>
+    const scrollTimeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
 
+    scrollTimeLine
+      .from(titleSpilt.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from(".top-grid div, .bottom-grid div", {
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        stagger: 0.04,
+      }, '-=0.5');
+  });
+  return (
+    <section id="about">
+      <div className="mb-16 md:px-0 px-5">
+        <div className="content">
+          <div className="md:col-span-8">
+            <p className="badge">Best Cocktails</p>
+            <h2>
+              where every details matters <span className="text-white">-</span>
+              from muddle to granish
+            </h2>
+          </div>
 
-                </div>
-                <div className='top-grid'>
-                    <div className='md:col-span-3'>
-                        <div className='noisy' />
-                        <img src='/images/abt1.png' alt='grid-img-1' />
-                    </div>
-
-                    <div className='md:col-span-6'>
-                        <div className='noisy' />
-                        <img src='/images/abt2.png' alt='grid-img-2' />
-                    </div>
-
-
-                    <div className='md:col-span-3'>
-                        <div className='noisy' />
-                        <img src='/images/abt5.png' alt='grid-img-5' />
-                    </div>
-
-
-                </div>
-
-                <div className='bottom-grid'>
-                    <div className='md:col-span-8'>
-                        <div className='noisy' />
-                        <img src='/images/abt3.png' alt='grid-img-3' />
-                    </div>
-
-
-                    <div className='md:col-span-4'>
-                        <div className='noisy' />
-                        <img src='/images/abt4.png' alt='grid-img-3' />
-                    </div>
-                </div>
+          <div className="sub-content">
+            <p>
+              Every cocktail we serve is a reflection of our obsession with
+              detail - from the first muddle to the final granish. That care is
+              what turns a simple drink into something truly memorable
+            </p>
+            <div>
+              <p className="md:text-3xl text-xl font-bold">
+                <span>4.5</span>/5
+              </p>
+              <p className=" text-sm text-white-100">
+                more than +12000 customers
+              </p>
             </div>
+          </div>
+        </div>
+        <div className="top-grid">
+          <div className="md:col-span-3">
+            <div className="noisy" />
+            <img src="/images/abt1.png" alt="grid-img-1" />
+          </div>
 
-        </section>
-    )
-}
+          <div className="md:col-span-6">
+            <div className="noisy" />
+            <img src="/images/abt2.png" alt="grid-img-2" />
+          </div>
 
-export default About
+          <div className="md:col-span-3">
+            <div className="noisy" />
+            <img src="/images/abt5.png" alt="grid-img-5" />
+          </div>
+        </div>
+
+        <div className="bottom-grid">
+          <div className="md:col-span-8">
+            <div className="noisy" />
+            <img src="/images/abt3.png" alt="grid-img-3" />
+          </div>
+
+          <div className="md:col-span-4">
+            <div className="noisy" />
+            <img src="/images/abt4.png" alt="grid-img-3" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
