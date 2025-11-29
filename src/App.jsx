85cb54fd00fc17577cdf70e7ff749c9815da1720
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-11-29 20:01:24 +0500

    about completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Concktails from "./components/Concktails"
+import About from "./components/About"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero/>
      <Concktails/>
+     <About/>
     </main>
   )
 }
