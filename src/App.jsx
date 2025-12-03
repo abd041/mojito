9e4f454dda94b27f8cfc46360ae9f7c1e0c6ec2a
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-12-04 00:51:01 +0500

    art section is inprogress

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Concktails from "./components/Concktails"
 import About from "./components/About"
+import Art from "./components/Art"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero/>
      <Concktails/>
      <About/>
+     <Art/>
     </main>
   )
 }
